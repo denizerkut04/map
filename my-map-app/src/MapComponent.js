af4f88807996c71ba58a45fc0ea543e98c15ef12
Deniz Erkut <denizerkut04@icloud.com>
2024-09-08 15:13:10 +0300

    leaflet eklendi

---
+++ my-map-app/src/MapComponent.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const MapComponent = () => {
+    const position = [51.505, -0.09]; // Haritanın merkezi
+
+    return (
+        <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={position}>
+                <Popup>
+                    A popup message here.
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+};
+
+export default MapComponent;
