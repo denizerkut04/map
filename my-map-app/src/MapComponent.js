df67c42bec99dc307a4612fa08e8030673075b68
Deniz Erkut <denizerkut04@icloud.com>
2024-09-08 16:05:59 +0300

    delaunay ucgeni  (voronai incele)

--- my-map-app/src/MapComponent.js
+++ my-map-app/src/MapComponent.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {MapContainer, TileLayer, Marker, Popup, Polyline} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import data from './turkiye.json'; // JSON dosyasını import et
+import Delaunator from 'delaunator'; // Default import
 
 const MapComponent = () => {
     const position = [39.0, 35.0]; // turkiye'nin merkezi koordinatları
     const zoomLevel = 6; // turkiye'nin tamamını göstermek için uygun bir zoom seviyesi
 
     const cities = data; //read usa.json
+
+    const ankaraCoords = cities.find(city => city.plaka === 6);
+    const istanbulCoords = cities.find(city => city.plaka === 34);
+    const lineAnkIst = [
+            [ankaraCoords.lat, ankaraCoords.lon],
+            [istanbulCoords.lat, istanbulCoords.lon]
+    ];
+
+    const coordinates = cities.map(city =>
+        [city.lat, city.lon]
+    ); // delaunay kullanimina uygun hale getirdik
+
+    // Delaunay Üçgenlemesi uygulama
+    const delaunay = Delaunator.from(coordinates);
+    const triangles = delaunay.triangles;
+
+    // Üçgenlerin köşe koordinatlarını çıkar
+    const lines = [];
+    for (let i = 0; i < triangles.length; i += 3) {
+        const triangle = [triangles[i], triangles[i + 1], triangles[i + 2], triangles[i]];
+        lines.push(triangle.map(index => coordinates[index]));
+    }
 
     return (
         <MapContainer center={position} zoom={zoomLevel} style={{ height: "100vh", width: "100%" }}>
@@ -23,6 +46,11 @@ const MapComponent = () => {
                         {city.plaka} {city.il_adi}
                     </Popup>
                 </Marker>
+            ))}
+            <Polyline positions={lineAnkIst} color="red" weight={3} />
+
+            {lines.map((line, index) => (
+                <Polyline key={index} positions={line} color="blue" weight={2} />
             ))}
 
         </MapContainer>
